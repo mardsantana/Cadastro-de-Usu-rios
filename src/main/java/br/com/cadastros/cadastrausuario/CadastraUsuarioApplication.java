650ffc1fa4ac1e8703158f035760f2858669e5e0
Mardson Santana <mardsonsantana105@gmail.com>
2023-04-21 03:32:12 -0300

    inicia projeto

---
+++ src/main/java/br/com/cadastros/cadastrausuario/CadastraUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastros.cadastrausuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastraUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastraUsuarioApplication.class, args);
+	}
+
+}
